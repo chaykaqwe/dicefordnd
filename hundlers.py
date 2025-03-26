1d41383116409f1c271063061932c4219947bb00
chaykaqwe <tatardastan@gmail.com>
2025-03-26 19:40:20 +0300

    Add files via upload
    added logging

--- hundlers.py
+++ hundlers.py
@@ -1,9 +1,20 @@
 from aiogram import Router, F
-from aiogram.filters import CommandStart, Command
+from aiogram.filters import CommandStart
 from aiogram.types import Message
 from random import randint
+import logging
 
 import app.keybord as kb
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - [%(levelname)s] - %(name)s - %(message)s",
+    handlers=[
+        logging.FileHandler("bot_logs.log", encoding="utf-8"),
+        logging.StreamHandler()
+    ]
+)
 
 
 router = Router(name=__name__)
@@ -11,7 +22,8 @@ router = Router(name=__name__)
 
 @router.message(CommandStart())
 async def comand_start(mes: Message):
-    await mes.answer_photo("https://i.etsystatic.com/21914118/r/il/c349cb/5618266017/il_794xN.5618266017_41ha.jpg",
+    logging.info(f"Пользователь {mes.from_user.username} ({mes.from_user.id}) запустил бота.")
+    await mes.answer_photo("https://ir.ozone.ru/s3/multimedia-t/c1000/6367003685.jpg",
                            caption='Добро пожаловать тут вы можите кидать кубики!', reply_markup=kb.main)
 
 
